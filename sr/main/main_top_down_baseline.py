7bcf60cc8406968e8d4328b3c2a766d720073ffd
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-09-13 16:07:36 +0800

    baseline codebase

--- sr/main/main_top_down_baseline.py
+++ sr/main/main_top_down_baseline.py
@@ -1,13 +1,11 @@
 import torch
 import json
 import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from .. import utils, imsitu_scorer, imsitu_loader, imsitu_encoder
+#from .. import utils, imsitu_scorer, imsitu_loader, imsitu_encoder
 import time
 
-from ..model import top_down_baseline
+from model import top_down_baseline
 
 
 def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, model_dir, encoder, gpu_mode, clip_norm, model_name, model_saving_name, eval_frequency=4000):
