80548da5f219769840f82ce425b3d7fefb4057e2
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-10-17 11:34:23 +0800

    add word embedding for cls weights

--- main_top_down_norm_cls_embed.py
+++ main_top_down_norm_cls_embed.py
@@ -26,11 +26,6 @@ def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, mode
     top1 = imsitu_scorer.imsitu_scorer(encoder, 1, 3)
     top5 = imsitu_scorer.imsitu_scorer(encoder, 5, 3)
 
-    print(model.classifier)
-    for f in model.classifier.parameters():
-        print(f.requires_grad)
-
-
     for epoch in range(max_epoch):
 
         for i, (_, img, verb, labels) in enumerate(train_loader):
@@ -179,7 +174,6 @@ def main():
     #init classifier weights
     cls_emb_path = 'data/glove6b_init_imsitu_carn.npy'
     weight_init = torch.from_numpy(np.load(cls_emb_path))
-    print(model.classifier.main[-1].weight.data.size(), weight_init.size())
     model.classifier.main[-1].weight.data.copy_(weight_init)
 
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=n_worker)
