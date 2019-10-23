0d293df373e8f3cb17a89439d19bc4be1733cc33
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-10-23 19:01:05 +0800

    query context new

--- main_top_down_baseline_with_contet_erased.py
+++ main_top_down_baseline_with_contet_erased.py
@@ -210,7 +210,7 @@ def main():
         utils.set_trainable(model.v_net, True)
         optimizer = torch.optim.Adamax([{'params': model.v_net.parameters()},
                                         {'params': model.classifier.parameters()}
-                                        ], lr=5e-5)
+                                        ], lr=1e-4)
         model_name = 'finetune_cls'
 
     else:
