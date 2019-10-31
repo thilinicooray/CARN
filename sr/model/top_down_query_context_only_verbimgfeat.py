c0566c1759cc7aa5a9fe131d467c94ae6519663d
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-10-31 10:00:32 +0800

    query role with verb img feat

--- sr/model/top_down_query_context_only_verbimgfeat.py
+++ sr/model/top_down_query_context_only_verbimgfeat.py
@@ -151,8 +151,9 @@ class Top_Down_Baseline(nn.Module):
             mfb_l2 = F.normalize(mfb_sign_sqrt)
             out = mfb_l2
 
-            gate = torch.sigmoid(q_list[-1].contiguous().view(-1, q_emb_mul_head.size(-1)*n_heads) * q_repr.contiguous().view(-1, q_emb_mul_head.size(-1)*n_heads))
-            print(gate.size(), out.size())
+            #gate = torch.sigmoid(q_list[-1].contiguous().view(-1, q_emb_mul_head.size(-1)*n_heads) * q_repr.contiguous().view(-1, q_emb_mul_head.size(-1)*n_heads))
+            #print(gate.size(), out.size())
+            gate = ans_list[-1] * out
             out = gate * ans_list[-1] + (1-gate) * out
 
             q_list.append(q_repr)
