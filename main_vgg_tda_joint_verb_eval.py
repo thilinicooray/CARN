b352e42b2eda42e3469968592b09d800d4dfe9b8
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-11-11 10:46:06 +0800

    topdown qctx role with pretrained baseline

---
+++ main_vgg_tda_joint_verb_eval.py
@@ -0,0 +1,284 @@
+import torch
+import json
+import os
+
+from sr import utils, imsitu_scorer, imsitu_scorer_rare, imsitu_loader, imsitu_encoder
+from sr.model import vgg_tda_joint_verb_eval, top_down_verb_pred_agentplace, single_role_vgg_classifier
+
+
+
+
+def eval(model, dev_loader, encoder, gpu_mode, write_to_file = False):
+    model.eval()
+
+    print ('evaluating model...')
+    top1 = imsitu_scorer.imsitu_scorer(encoder, 1, 3, write_to_file)
+    top5 = imsitu_scorer.imsitu_scorer(encoder, 5, 3)
+    with torch.no_grad():
+
+        for i, (img_id, img, verb_pred, verb) in enumerate(dev_loader):
+
+            #print(img_id[0], encoder.verb2_role_dict[encoder.verb_list[verb[0]]])
+
+            if gpu_mode >= 0:
+                img = torch.autograd.Variable(img.cuda())
+                verb_pred = torch.autograd.Variable(verb_pred.cuda())
+                verb = torch.autograd.Variable(verb.cuda())
+            else:
+                img = torch.autograd.Variable(img)
+                verb_pred = torch.autograd.Variable(verb_pred)
+                verb = torch.autograd.Variable(verb)
+
+            verb_predict = model(img, verb_pred)
+
+            top1.add_point_verb_only_eval(img_id, verb_predict, verb)
+            top5.add_point_verb_only_eval(img_id, verb_predict, verb)
+
+            del verb_predict, img, verb
+            #break
+
+    return top1, top5, 0
+
+
+
+def eval_rare(model, dev_loader, encoder, gpu_mode, image_group = {}):
+    model.eval()
+
+    print ('evaluating rare portion ...')
+    top1 = imsitu_scorer_rare.imsitu_scorer(encoder, 1, 3, image_group)
+    top5 = imsitu_scorer_rare.imsitu_scorer(encoder, 5, 3, image_group)
+    with torch.no_grad():
+
+        for i, (img_id, img, verb, labels) in enumerate(dev_loader):
+
+            #print(img_id[0], encoder.verb2_role_dict[encoder.verb_list[verb[0]]])
+
+            if gpu_mode >= 0:
+                img = torch.autograd.Variable(img.cuda())
+                verb = torch.autograd.Variable(verb.cuda())
+                labels = torch.autograd.Variable(labels.cuda())
+            else:
+                img = torch.autograd.Variable(img)
+                verb = torch.autograd.Variable(verb)
+                labels = torch.autograd.Variable(labels)
+
+            role_predict = model(img, verb)
+
+            top1.add_point_noun_log(img_id, verb, role_predict, labels)
+            top5.add_point_noun_log(img_id, verb, role_predict, labels)
+
+            del role_predict, img, verb, labels
+            #break
+
+    return top1, top5, 0
+
+def main():
+
+    import argparse
+    parser = argparse.ArgumentParser(description="imsitu VSRL. Training, evaluation and prediction.")
+    parser.add_argument("--gpuid", default=-1, help="put GPU id > -1 in GPU mode", type=int)
+    parser.add_argument('--output_dir', type=str, default='./trained_models', help='Location to output the model')
+    parser.add_argument('--resume_training', action='store_true', help='Resume training from the model [resume_model]')
+    parser.add_argument('--resume_model', type=str, default='', help='The model we resume')
+    parser.add_argument('--evaluate', action='store_true', help='Only use the testing mode')
+    parser.add_argument('--evaluate_rare', action='store_true', help='Only use the testing mode')
+    parser.add_argument('--test', action='store_true', help='Only use the testing mode')
+    parser.add_argument('--dataset_folder', type=str, default='./imSitu', help='Location of annotations')
+    parser.add_argument('--imgset_dir', type=str, default='./resized_256', help='Location of original images')
+    parser.add_argument('--train_file', default="train_freq2000.json", type=str, help='trainfile name')
+    parser.add_argument('--dev_file', default="dev_freq2000.json", type=str, help='dev file name')
+    parser.add_argument('--test_file', default="test_freq2000.json", type=str, help='test file name')
+    parser.add_argument('--org_train_file', default="train.json", type=str, help='org train file name')
+    parser.add_argument('--org_test_file', default="test.json", type=str, help='org test file name')
+    parser.add_argument('--model_saving_name', type=str, help='saving name of the outpul model')
+
+    parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--model', type=str, default='vgg_tda_verb_joint')
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--seed', type=int, default=1111, help='random seed')
+    parser.add_argument('--clip_norm', type=float, default=0.25)
+    parser.add_argument('--num_workers', type=int, default=3)
+
+    parser.add_argument('--vgg_model', type=str, default='', help='Pretrained vgg model')
+    parser.add_argument('--tda_model', type=str, default='', help='Pretrained topdown model')
+
+    args = parser.parse_args()
+
+    n_epoch = args.epochs
+    batch_size = args.batch_size
+    clip_norm = args.clip_norm
+    n_worker = args.num_workers
+
+    dataset_folder = args.dataset_folder
+    imgset_folder = args.imgset_dir
+
+    train_set = json.load(open(dataset_folder + '/' + args.train_file))
+
+    encoder = imsitu_encoder.imsitu_encoder(train_set)
+
+    train_set = imsitu_loader.imsitu_loader_top_down_verb(imgset_folder, train_set, encoder,'train', encoder.train_transform)
+
+    constructor = 'build_single_role_classifier'
+    vgg_model = getattr(single_role_vgg_classifier, constructor)(len(encoder.verb_list))
+
+
+    constructor = 'build_top_down_baseline_verb'
+    tda_model = getattr(top_down_verb_pred_agentplace, constructor)((encoder.get_num_labels(),  encoder.get_num_verbs(), vgg_model))
+
+
+
+    constructor = 'build_%s' % args.model
+    model = getattr(vgg_tda_joint_verb_eval, constructor)(vgg_model, tda_model)
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=n_worker)
+
+    dev_set = json.load(open(dataset_folder + '/' + args.dev_file))
+    dev_set = imsitu_loader.imsitu_loader_top_down_verb(imgset_folder, dev_set, encoder, 'val', encoder.dev_transform)
+    dev_loader = torch.utils.data.DataLoader(dev_set, batch_size=batch_size, shuffle=True, num_workers=n_worker)
+
+    test_set = json.load(open(dataset_folder + '/' + args.test_file))
+    test_set = imsitu_loader.imsitu_loader_top_down_verb(imgset_folder, test_set, encoder, 'test', encoder.dev_transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=n_worker)
+
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    torch.manual_seed(args.seed)
+    if args.gpuid >= 0:
+        model.cuda()
+        torch.cuda.manual_seed(args.seed)
+        torch.backends.cudnn.benchmark = True
+
+
+    #load models
+    utils.load_net(args.baseline_model, [model.baseline_model])
+    utils.load_net(args.qctx_model, [model.qctx_model])
+
+
+
+    if args.evaluate:
+        top1, top5, val_loss = eval(model, dev_loader, encoder, args.gpuid, write_to_file = True)
+
+        top1_avg = top1.get_average_results()
+        top5_avg = top5.get_average_results()
+
+        avg_score = top1_avg["verb"] + top1_avg["value"] + top1_avg["value-all"] + top5_avg["verb"] + \
+                    top5_avg["value"] + top5_avg["value-all"] + top5_avg["value*"] + top5_avg["value-all*"]
+        avg_score /= 8
+
+        print ('Dev average :{:.2f} {} {}'.format( avg_score*100,
+                                                   utils.format_dict(top1_avg,'{:.2f}', '1-'),
+                                                   utils.format_dict(top5_avg, '{:.2f}', '5-')))
+
+        #write results to csv file
+        role_dict = top1.all_res
+        fail_val_all = top1.value_all_dict
+        pass_val_dict = top1.vall_all_correct
+
+        '''with open(args.model_saving_name+'_role_pred_data.json', 'w') as fp:
+            json.dump(role_dict, fp, indent=4)'''
+
+        '''with open(args.model_saving_name+'_fail_val_all.json', 'w') as fp:
+            json.dump(fail_val_all, fp, indent=4)
+
+        with open(args.model_saving_name+'_pass_val_all.json', 'w') as fp:
+            json.dump(pass_val_dict, fp, indent=4)'''
+
+        print('Writing predictions to file completed !')
+
+
+    elif args.evaluate_rare:
+
+        org_train_set = json.load(open(dataset_folder + '/' + args.org_train_file))
+        #compute sparsity statistics
+        verb_role_noun_freq = {}
+        for image,frames in org_train_set.items():
+            v = frames["verb"]
+            items = set()
+            for frame in frames["frames"]:
+                for (r,n) in frame.items():
+                    key = (v,r,n)
+                    items.add(key)
+            for key in items:
+                if key not in verb_role_noun_freq: verb_role_noun_freq[key] = 0
+                verb_role_noun_freq[key] += 1
+                #per role it is the most frequent prediction
+                #and among roles its the most rare
+
+        org_eval_dataset = json.load(open(dataset_folder + '/' + args.org_test_file))
+        image_sparsity = {}
+        for image,frames in org_eval_dataset.items():
+            v = frames["verb"]
+            role_max = {}
+            for frame in frames["frames"]:
+                for (r,n) in frame.items():
+                    key = (v,r,n)
+                    if key not in verb_role_noun_freq: freq = 0
+                    else: freq = verb_role_noun_freq[key]
+                    if r not in role_max or role_max[r] < freq: role_max[r] = freq
+            min_val = -1
+            for (r,f) in role_max.items():
+                if min_val == -1 or f < min_val: min_val = f
+            image_sparsity[image] = min_val
+
+        sparsity_max = 10
+        x = range(0, sparsity_max+1)
+        print ("evaluating images where most rare verb-role-noun in training is x , s.t. {} <= x <= {}".format(0, sparsity_max))
+        n = 0
+        for (k,v) in image_sparsity.items():
+            if v in x:
+                n+=1
+        print ("total images = {}".format(n))
+
+        top1, top5, val_loss = eval_rare(model, test_loader, encoder, args.gpuid, image_sparsity)
+
+        top1_avg = top1.get_average_results(range(0, sparsity_max+1))
+        top5_avg = top5.get_average_results(range(0, sparsity_max+1))
+
+        avg_score = top1_avg["verb"] + top1_avg["value"] + top1_avg["value-all"] + top5_avg["verb"] + \
+                    top5_avg["value"] + top5_avg["value-all"] + top5_avg["value*"] + top5_avg["value-all*"]
+        avg_score /= 8
+
+        print ('Test rare average :{:.2f} {} {}'.format( avg_score*100,
+                                                         utils.format_dict(top1_avg,'{:.2f}', '1-'),
+                                                         utils.format_dict(top5_avg, '{:.2f}', '5-')))
+
+
+
+
+    elif args.test:
+        top1, top5, val_loss = eval(model, test_loader, encoder, args.gpuid, write_to_file = True)
+
+        top1_avg = top1.get_average_results()
+        top5_avg = top5.get_average_results()
+
+        avg_score = top1_avg["verb"] + top1_avg["value"] + top1_avg["value-all"] + top5_avg["verb"] + \
+                    top5_avg["value"] + top5_avg["value-all"] + top5_avg["value*"] + top5_avg["value-all*"]
+        avg_score /= 8
+
+        print ('Test average :{:.2f} {} {}'.format( avg_score*100,
+                                                    utils.format_dict(top1_avg,'{:.2f}', '1-'),
+                                                    utils.format_dict(top5_avg, '{:.2f}', '5-')))
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sr/model/vgg_tda_joint_verb_eval.py
@@ -0,0 +1,76 @@
+'''
+This is the baseline model.
+We directly use bottom up VQA like mechanism for SR
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from torch.nn.utils.weight_norm import weight_norm
+import copy
+
+from ..lib.attention import Attention
+from ..lib.classifier import SimpleClassifier
+from ..lib.fc import FCNet
+import torchvision as tv
+from ..utils import cross_entropy_loss
+
+class vgg16_modified(nn.Module):
+    def __init__(self):
+        super(vgg16_modified, self).__init__()
+        vgg = tv.models.vgg16_bn(pretrained=True)
+        self.vgg_features = vgg.features
+
+    def forward(self,x):
+        features = self.vgg_features(x)
+        return features
+
+def attention(query, key, value, mask=None, dropout=None):
+    "Compute 'Scaled Dot Product Attention'"
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) \
+             / math.sqrt(d_k)
+
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = F.softmax(scores, dim = -1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+
+    return torch.matmul(p_attn, value), p_attn , torch.mean(scores,1)
+
+class Top_Down_Baseline(nn.Module):
+    def __init__(self, vgg_model, tda_model):
+        super(Top_Down_Baseline, self).__init__()
+        self.vgg_model = vgg_model
+        self.tda_model = tda_model
+
+    def forward(self, v_org, gt_verb, verb_role_impact):
+
+        vgg_out = self.vgg_model(v_org, gt_verb)
+        tda_out = self.tda_model(v_org, gt_verb)
+
+        #print(verb_role_impact.size(), verb_role_impact[:5], qctx_out.size(), baseline_out.size())
+        '''q_impact = verb_role_impact.unsqueeze(-1)
+        b_impact = torch.ones(q_impact.size(0), q_impact.size(1), 1).cuda() - q_impact'''
+
+        verb_pred = tda_out + vgg_out
+
+        #role_label_pred = logits.contiguous().view(v.size(0), self.encoder.max_role_count, -1)
+
+        return verb_pred
+
+
+
+
+
+
+
+def build_vgg_tda_verb_joint(vgg_model, tda_model):
+
+
+
+    return Top_Down_Baseline(vgg_model, tda_model)
+
+
