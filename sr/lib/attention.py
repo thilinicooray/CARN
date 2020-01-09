185e2a3bfe29377f4ea6b4150975a113ac0e3af5
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-01-09 09:58:30 +0800

    check ctx mask att

--- sr/lib/attention.py
+++ sr/lib/attention.py
@@ -37,6 +37,47 @@ class Attention(nn.Module):
         w = nn.functional.softmax(logits, 1)
         return w
 
+    def forward_with_ctxatt_mask(self, v, q, mask):
+        """
+        v: [batch, k, vdim]
+        q: [batch, qdim]
+        """
+        logits = self.logits(v, q)
+        w = logits
+        single_att = nn.functional.softmax(logits, 1)
+
+        w = w.contiguous().view(mask.size(0), mask.size(1), -1)
+        w_ctx = w * mask.unsqueeze(-1)
+
+        required_indices = [[1,2,3,4,5],[0,2,3,4,5],[0,1,3,4,5],[0,1,2,4,5],[0,1,2,3,5],[0,1,2,3,4]]
+
+        sum_att = torch.ones(mask.size(0), w.size(-1))
+        if torch.cuda.is_available():
+            sum_att = sum_att.to(torch.device('cuda'))
+
+        updated_att = None
+
+        for rolei in range(w.size(1)):
+            current_indices = required_indices[rolei]
+            current_indices = torch.tensor(current_indices)
+            if torch.cuda.is_available():
+                current_indices = current_indices.to(torch.device('cuda'))
+
+            neighbour_att = torch.sum(torch.index_select(w_ctx, 1, current_indices),1)
+            neighbour_removed_att = sum_att - neighbour_att
+
+            updated_cur_role_att = nn.functional.softmax(neighbour_removed_att + w[:,rolei],1)
+
+            if rolei == 0:
+                updated_att = updated_cur_role_att.unsqueeze(1)
+            else:
+                updated_att = torch.cat((updated_att.clone(), updated_cur_role_att.unsqueeze(1)), 1)
+
+
+        context_erased_att = updated_att.contiguous().view(-1, updated_att.size(-1), 1)
+
+        return single_att, context_erased_att
+
     def logits(self, v, q):
         num_objs = v.size(1)
         q = q.unsqueeze(1).repeat(1, num_objs, 1)
---
+++ sr/model/td_pretrained_baseline_ctxatt_mask_cnn_reuse.py
@@ -0,0 +1,113 @@
+'''
+This is the baseline model.
+We directly use bottom up VQA like mechanism for SR
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ..lib.attention import Context_Erased_Attention
+from ..lib.classifier import SimpleClassifier
+from ..lib.fc import FCNet
+import torchvision as tv
+from ..utils import cross_entropy_loss
+
+class vgg16_modified(nn.Module):
+    def __init__(self):
+        super(vgg16_modified, self).__init__()
+        vgg = tv.models.vgg16_bn(pretrained=True)
+        self.vgg_features = vgg.features
+
+    def forward(self,x):
+        features = self.vgg_features(x)
+        return features
+
+class Top_Down_Baseline(nn.Module):
+    def __init__(self, baseline_model, encoder, Dropout_C, obj_cls):
+        super(Top_Down_Baseline, self).__init__()
+        self.baseline_model = baseline_model
+        self.encoder = encoder
+        self.Dropout_C = Dropout_C
+        self.obj_cls = obj_cls
+
+    def forward(self, v_org, gt_verb):
+
+        role_oh_encoding = self.encoder.get_verb2role_encoing_batch(gt_verb)
+        if torch.cuda.is_available():
+            role_oh_encoding = role_oh_encoding.to(torch.device('cuda'))
+
+        img_features = self.baseline_model.convnet(v_org)
+        batch_size, n_channel, conv_h, conv_w = img_features.size()
+
+        img_org = img_features.view(batch_size, -1, conv_h* conv_w)
+        v = img_org.permute(0, 2, 1)
+
+        batch_size = v.size(0)
+
+        role_idx = self.encoder.get_role_ids_batch(gt_verb)
+
+        if torch.cuda.is_available():
+            role_idx = role_idx.to(torch.device('cuda'))
+
+        img = v
+
+        img = img.expand(self.encoder.max_role_count, img.size(0), img.size(1), img.size(2))
+
+        img = img.transpose(0,1)
+        img = img.contiguous().view(batch_size * self.encoder.max_role_count, -1, v.size(2))
+
+        verb_embd = self.baseline_model.verb_emb(gt_verb)
+        role_embd = self.baseline_model.role_emb(role_idx)
+
+        verb_embed_expand = verb_embd.expand(self.encoder.max_role_count, verb_embd.size(0), verb_embd.size(1))
+        verb_embed_expand = verb_embed_expand.transpose(0,1)
+        concat_query = torch.cat([ verb_embed_expand, role_embd], -1)
+        role_verb_embd = concat_query.contiguous().view(-1, role_embd.size(-1)*2)
+        q_emb = self.baseline_model.query_composer(role_verb_embd)
+
+        att, context_erased_att = self.baseline_model.v_att.forward_with_ctxatt_mask(img, q_emb, role_oh_encoding)
+        ctx_erased_v_emb = (context_erased_att * img).sum(1)
+
+        logits_obj = self.obj_cls(ctx_erased_v_emb)
+
+        logits = logits_obj
+
+        role_label_pred = logits.contiguous().view(v.size(0), self.encoder.max_role_count, -1)
+
+        return role_label_pred
+
+    def calculate_loss(self, gt_verbs, role_label_pred, gt_labels):
+
+        batch_size = role_label_pred.size()[0]
+        criterion = nn.CrossEntropyLoss(ignore_index=self.encoder.get_num_labels())
+
+        gt_label_turned = gt_labels.transpose(1,2).contiguous().view(batch_size* self.encoder.max_role_count*3, -1)
+
+        role_label_pred = role_label_pred.contiguous().view(batch_size* self.encoder.max_role_count, -1)
+        role_label_pred = role_label_pred.expand(3, role_label_pred.size(0), role_label_pred.size(1))
+        role_label_pred = role_label_pred.transpose(0,1)
+        role_label_pred = role_label_pred.contiguous().view(-1, role_label_pred.size(-1))
+
+        loss = criterion(role_label_pred, gt_label_turned.squeeze(1)) * 3
+
+        return loss
+
+def build_top_down_baseline(n_roles, n_verbs, num_ans_classes, encoder, baseline_model):
+
+    hidden_size = 1024
+    img_embedding_size = 512
+
+    obj_cls = nn.Sequential(
+        nn.Linear(img_embedding_size, hidden_size*2),
+        nn.BatchNorm1d(hidden_size*2),
+        nn.ReLU(),
+        nn.Dropout(0.5),
+        nn.Linear(hidden_size*2, num_ans_classes)
+    )
+
+    Dropout_C = nn.Dropout(0.1)
+
+    return Top_Down_Baseline(baseline_model, encoder, Dropout_C, obj_cls)
+
+
