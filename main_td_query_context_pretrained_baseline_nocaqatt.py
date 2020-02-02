cbaccf2f12d5636c0f2d47ee77c69cc24252c500
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-02-02 23:08:59 +0800

    caq no att

--- main_td_query_context_pretrained_baseline_nocaqatt.py
+++ main_td_query_context_pretrained_baseline_nocaqatt.py
@@ -7,7 +7,7 @@ from sr import utils, imsitu_scorer, imsitu_scorer_rare, imsitu_loader, imsitu_e
 from sr.model import top_down_query_context_pretrained_baseline_nocaqatt, top_down_baseline
 
 
-def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, model_dir, encoder, gpu_mode, clip_norm, model_name, model_saving_name, eval_frequency=4):
+def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, model_dir, encoder, gpu_mode, clip_norm, model_name, model_saving_name, eval_frequency=4000):
     model.train()
 
     time_all = time.time()
@@ -131,7 +131,7 @@ def eval(model, dev_loader, encoder, gpu_mode, write_to_file = False):
                 top5.add_point_noun(verb, role_predict, labels)
 
             del role_predict, img, verb, labels
-            break
+            #break
 
     return top1, top5, 0
 
