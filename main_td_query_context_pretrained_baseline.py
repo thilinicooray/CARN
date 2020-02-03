d96b1e41ffa88dff110b7cfb01e528983a5d2e00
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-02-03 20:50:10 +0800

    caq no att

--- main_td_query_context_pretrained_baseline.py
+++ main_td_query_context_pretrained_baseline.py
@@ -30,8 +30,9 @@ def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, mode
     all = count_parameters(model)
     cnn = count_parameters(model.convnet)
     base = count_parameters(model.baseline_model)
+    neighbour = count_parameters(model.neighbour_attention)
 
-    print('model parameters - all, cnn, base ', all, cnn, base)
+    print('model parameters - all, cnn, base ', all, cnn, base, neighbour)
 
 
     top1 = imsitu_scorer.imsitu_scorer(encoder, 1, 3)
